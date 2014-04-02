34c23a95d622dc46f9ff54000a6e49406ca0cca3
Maxim Shchetynin <maxim@macbook-pro-elena.fritz.box>
2014-04-02 07:33:18 +0200

    Initial Commit

---
+++ Spectator/HALAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HALAppDelegate.h
+//  Spectator
+//
+//  Created by Maxim Shchetynin on 02.04.14.
+//  Copyright (c) 2014 Maxim Shchetynin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HALAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Spectator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Spectator
+//
+//  Created by Maxim Shchetynin on 02.04.14.
+//  Copyright (c) 2014 Maxim Shchetynin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HALAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HALAppDelegate class]));
+    }
+}
